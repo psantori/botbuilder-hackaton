7e7c0cb5c8ecdc3be31ec0ebac7a5bc952f5fe6d
Giada Delli <delli.giada@gmail.com>
2018-02-28 16:19:57 +0100

    delete appointment

---
+++ modules/delete-intent.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const parseIntent = (userState, luisIntent) => {
+    luisIntent.entities.forEach(ent => {
+        switch(ent.type) {
+            case 'agent':
+                userState['intent'].agentId = ent.entity;
+            break;
+            case 'location':
+                userState['intent'].storeId = ent.entity;
+            break;
+            case 'builtin.datetimeV2.date':
+                userState['intent'].date = ent.resolution.values.pop().value; 
+            break;
+            
+            default:
+        }
+    });
+    return true;
+}
+
+const checkEntities = (context, userState, luisIntent) => {
+    return new Promise((resolve, reject) => {
+        parseIntent(userState, luisIntent);
+        let missing = null;
+        if (!userState['intent'].date) {
+            missing = 'Ok, when you set the appointment?';
+        } 
+        /*else if (!userState['intent'].storeId) {
+            missing = 'Well, what Store do you prefer';
+        } else if (!userState['intent'].agentId) {
+            missing = 'Do you want select Store Assistant?';
+        }*/
+        if (missing) {
+            context.reply(missing);
+            resolve(false);
+        } else {
+            resolve(true);
+        }
+    });
+}
+
+module.exports = {
+    checkEntities
+}
