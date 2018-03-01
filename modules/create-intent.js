6f9820d32b3e4e7f9c245331524c985373932067
Giada Delli <delli.giada@gmail.com>
2018-03-01 11:09:34 +0100

    fixed bug on create app.

--- modules/create-intent.js
+++ modules/create-intent.js
@@ -15,12 +15,15 @@ const parseIntent = (userState, luisIntent) => {
         switch(ent.type) {
             case 'agent':
                 userState['intent'].agentId = ent.entity;
+                userState['promptStatus'] = undefined;
             break;
             case 'location':
                 userState['intent'].storeId = ent.entity;
+                userState['promptStatus'] = undefined;
             break;
             case 'builtin.datetimeV2.date':
                 userState['intent'].date = ent.resolution.values.pop().value; 
+                userState['promptStatus'] = undefined;
             break;
             
             default:
@@ -35,11 +38,14 @@ const checkEntities = (context, userState, luisIntent) => {
         let missing = null;
         if (!userState['intent'].date) {
             missing = 'Ok, when you want set the appointment?';
+            context.state.user['promptStatus'] = 'ask-date';
         } else if (!userState['intent'].storeId) {
             missing = 'Well, what Store do you prefer';
+            context.state.user['promptStatus'] = 'ask-store';
         } else if (!userState['intent'].agentId) {
-            if (luisIntent.name != 'None') {
+            if (!context.state.user['promptStatus']) {
                 missing = 'Do you want select Store Assistant?';
+                context.state.user['promptStatus'] = 'ask-sa';
             } else if (context.request.text.toLowerCase() == 'yes') {
                 missing = MessageStyler.carousel([
                     CardStyler.heroCard(null, ['https://randomuser.me/api/portraits/women/44.jpg'], ['Vivian']),
